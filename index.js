81fd97bd8e86869518cf5b10ad567df6ad3580c4
Cristhian Buiza Infantes <c.buiza@rpalatam.com.pe>
2023-06-15 14:29:20 -0500

    feat: se agrego readline

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import fetch from "node-fetch";
 import chalk from "chalk";
 import clipboard from "clipboardy";
+import readline from "readline";
 
 dotenv.config();
 
@@ -23,10 +24,15 @@ const headersSandbox = {
   "Content-Type": "application/json",
 };
 
-(async () => {
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter the ID: ", async (id) => {
   try {
     const response = await fetch(
-      `${BASE_PROD}/draft/v1/story/AVJWV4LOTZF7PA72Y74AVKFHOI/revision/published`,
+      `${BASE_PROD}/draft/v1/story/${id}/revision/published`,
       {
         method: "GET",
         headers,
@@ -36,15 +42,19 @@ const headersSandbox = {
     const nota = JSON.stringify(ans.ans);
     // IF YOU WANT TO COPY IN YOUR CLIPBOARD
     // clipboard.writeSync(nota);
-    console.log(chalk.green("NOTA COPIADA!"));
+    // console.log(chalk.green("NOTA COPIADA!"));
     const sandboxResponse = await fetch(`${BASE_SANDBOX}/draft/v1/story/`, {
       method: "POST",
       body: nota,
       headers: headersSandbox,
     });
     const res = await sandboxResponse.json();
-    console.log(chalk.green("ID DE LA NOTA: ") + res.id);
+    console.log(chalk.green("--------------------------------"));
+    console.log(chalk.green("ID DE LA NOTA EN SANDBOX: ") + res.id);
+    console.log(chalk.green("--------------------------------"));
   } catch (err) {
     console.log(err);
   }
-})();
+
+  rl.close();
+});
